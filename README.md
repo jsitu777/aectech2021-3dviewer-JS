cafeb910bd22947d3f3465d13dfc970f251ac9da
wenjinsitu <59303945+wenjinsitu@users.noreply.github.com>
2021-11-06 21:13:53 -0700

    config node server, vue, threejs viewer

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# aectech2021-3dviewer-JS
+# aectech2021-3dviewer
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div id="threeContainer">
+      <Three />
+    </div>
+    <h1>HelloWorld</h1>
+    <div>
+      <p>Test your backend connection.</p>
+      <input type="button" value="Check" @click="runCheck" />
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Three from "./Three.vue";
+export default {
+  components: {
+    Three
+  },
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    runCheck() {
+      console.log("Making request");
+      fetch("/api/test")
+        .then(data => data.text())
+        .then(res => (this.msg = res));
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#threeContainer {
+  height: 600px;
+  width: 100%;
+}
+</style>
